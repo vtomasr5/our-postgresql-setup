ad525dbe49bc305323d13d8f5ccb24fad61d8783
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2019-01-05 13:20:03 +0100

    remove third tmux

--- tmux-session.sh
+++ tmux-session.sh
@@ -6,7 +6,6 @@ ARG=$1
 
 function setup-session() {
     tmux split-window
-    tmux split-window
     tmux select-layout even-horizontal
     # Handle tmux configs where windows aren't 0-indexed
     local tmux_first_window="$(tmux display-message -p '#I')"
@@ -15,10 +14,8 @@ function setup-session() {
     # Handle tmux configs where panes aren't 0-indexed
     local tmux_pane_1="$(tmux list-panes | cut -d':' -f1 | sed -n '1p')"
     local tmux_pane_2="$(tmux list-panes | cut -d':' -f1 | sed -n '2p')"
-    local tmux_pane_3="$(tmux list-panes | cut -d':' -f1 | sed -n '3p')"
     tmux send-keys -t pg:"${tmux_first_window}"."${tmux_pane_1}" C-m "vagrant up pg01 && vagrant ssh pg01" C-m
     tmux send-keys -t pg:"${tmux_first_window}"."${tmux_pane_2}" C-m "vagrant up pg02 && vagrant ssh pg02" C-m
-    tmux send-keys -t pg:"${tmux_first_window}"."${tmux_pane_3}" C-m "vagrant up pg03 && vagrant ssh pg03" C-m
     tmux attach-session -t pg
 }
 
